17d1a9c5c8717dedb2a4bcad566488c89efc9df5
barabas007 <shop.ru.shop>
2019-08-29 22:35:35 +0300

    4.1. Глупый бот.[#172472]

---
+++ chapter_001/src/main/java/ru/job4j/conditoin/DummyBot.java
@@ -0,0 +1,31 @@
+package ru.job4j.conditoin;
+
+public class DummyBot {
+
+
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+
+        System.out.println(answer("Привет Бот."));
+        System.out.println( answer("Пока."));
+        System.out.println(answer("Сколько будет 2 + 2?"));
+
+
+    }
+}
