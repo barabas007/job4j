2492bb967473d6b6523ea3020b7b4113979e11e9
barabas007 <shop.ru.shop>
2019-08-31 19:44:07 +0300

    add SqArea.java

--- chapter_001/src/main/java/ru/job4j/conditoin/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/conditoin/SqArea.java
@@ -1,27 +1,10 @@
 package ru.job4j.conditoin;
 
 public class SqArea {
-
-    public static double square(int p, int k){
-
-        // S = L * h
-        // h = p / 2 * (k + 1)
-        // L = h * k;
-
-        // L = (p/2 * (k + 1)) * k
-        // S = (p/2 * (k + 1) * k) * (p/2 * (k + 1))
-
-        return (p/2 * (k +1) * k) * (p/2 * (k + 1));
-    }
-
-
-    public static void main(String[] args) {
-
-        int p = 6;
-        int k = 2;
-        double expected = 162;
-        double out = square(p, k);
-        boolean passed = expected == out;
-        System.out.println(" p = 6, k = 2, s = 2, real = " + passed);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
