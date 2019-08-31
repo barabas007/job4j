61dabede8d8f5424b68bb9e5de2cac92db0c0551
barabas007 <shop.ru.shop>
2019-08-31 18:25:37 +0300

    4.3. Вычисление площади треугольника[#172478] Triangle.java

---
+++ chapter_001/src/main/java/ru/job4j/conditoin/Triangle.java
@@ -0,0 +1,9 @@
+package ru.job4j.conditoin;
+
+public class Triangle {
+
+    public static boolean exist(double ab, double ac, double dc) {
+
+        return ((ab + ac) > dc && (ac + dc) > ab && (ab + ab + dc) > ac);
+    }
+}
