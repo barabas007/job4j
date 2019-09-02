1e6cdef1573e6ef240582499c4437c19a532d9f1
barabas007 <shop.ru.shop>
2019-09-02 13:32:26 +0300

    5.4. Протеиновая диета.[#172488] add FitnessTest.java

---
+++ chapter_001/src/test/java/ru/job4j/condition/loop/FitnessTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import  ru.job4j.loop.Fitness;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(95, 90);
+        assertThat(days, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(90, 95);
+        assertThat(days, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        Fitness fit = new Fitness();
+        int days = fit.calc(50, 90);
+        assertThat(days, is(2));
+    }
+}
