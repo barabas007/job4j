3a983531d4ec57f5d1962f444861b1c54c39c596
barabas007 <shop.ru.shop>
2019-09-11 14:32:17 +0300

    5.7. Ипотека.[#172490] Mortgage.java

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,12 +4,15 @@ public class Mortgage {
     public int year(int amount, int monthly, double percent) {
         int year = 0;
 
+        int summ = amount;
+        while (summ > 0) {
+            summ = (int) (summ + summ * percent / 100);
+            summ = summ - monthly * 12;
+            year++;
+        }
 
 
-       while ( amount * percent < monthly * 12){
 
-          year++;
-       }
         return year;
     }
 
