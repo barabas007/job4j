bf948740ad9b2bd579e34a2e11e01fde28c06296
barabas007 <shop.ru.shop>
2019-09-18 14:59:28 +0300

    6.5.1. Сортировка выборкой.[#172506]FindLoop.java

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -17,7 +17,7 @@ public class FindLoop {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
 
         for (int index = start; index != finish; index++) {
-            if(data[index] == el){
+            if (data[index] == el) {
 
                 rst = index;
 
@@ -26,5 +26,23 @@ public class FindLoop {
 
         }
         return rst;
+    }
+
+    public int[]sort(int[] data) {
+
+        for (int left = 0; left < data.length; left++) {
+            int minIndex = left;
+            for (int right = left + 1; right < data.length; right++) {
+                if (data[right] < data[minIndex]) {
+                    minIndex = right;
+                }
+            }
+            if (minIndex != left) {
+                int temp = data[left];
+                data[left] = data[minIndex];
+                data[minIndex] = temp;
+            }
+        }
+        return data;
     }
 }
