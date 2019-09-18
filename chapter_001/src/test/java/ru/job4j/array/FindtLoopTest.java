7fa3c4c2e56a3d147ca5cfbe81fbc355ab5a249c
barabas007 <shop.ru.shop>
2019-09-18 15:00:33 +0300

    6.5.1. Сортировка выборкой.[#172506]FindLoopTest.java

--- chapter_001/src/test/java/ru/job4j/array/FindtLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindtLoopTest.java
@@ -1,8 +1,11 @@
 package ru.job4j.array;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import ru.job4j.array.FindLoop;
+@Ignore
 
 public class FindtLoopTest {
 
@@ -73,7 +76,7 @@ public class FindtLoopTest {
     @Test
     public void whenFind1() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 2, 10, 2, 4, 8, 3, 7, 9};
+        int[] input = new int[]{5, 2, 10, 2, 4, 8, 3, 7, 9};
         int value = 1;
         int start = 0;
         int finish = 8;
@@ -81,4 +84,42 @@ public class FindtLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenArrayHasLengh5Then8() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort6() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {7, 4, 3, 2, 1, 9};
+        int result[] = find.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 7, 9};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {3, 7, 0};
+        int result[] = find.sort(input);
+        int[] expect = new int[] {0, 3, 7};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {3, 4, 1, 2, 5};
+        int result[] = find.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
 }
