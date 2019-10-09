ba4e99c684548c4f7fee961af22dc34b74827e78
barabas007 <shop.ru.shop>
2019-10-09 15:55:15 +0300

    add Battery.java

--- chapter_002/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -7,12 +7,16 @@ public class Battery {
         this.load = size;
     }
     public void exchange(Battery another){
+
         this.load = this.load + another.load;
+        another.load = 0;
     }
 
     public static void main(String[] args) {
         Battery battery = new Battery(8);
         Battery battery1 = new Battery(9);
+        System.out.println("battery : " + battery.load + ". battery1 : " + battery1.load);
         battery.exchange(battery1);
+        System.out.println("battery : " + battery.load + ". battery1 : " + battery1.load);
     }
 }
