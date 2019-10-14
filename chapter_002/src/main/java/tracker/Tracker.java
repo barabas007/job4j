acb530044c8c892d3fe6314e3f72dfee8647e05b
barabas007 <shop.ru.shop>
2019-10-14 14:46:23 +0300

    Tracker

--- chapter_002/src/main/java/tracker/Tracker.java
+++ chapter_002/src/main/java/tracker/Tracker.java
@@ -1,5 +1,38 @@
 package tracker;
+/**
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class Tracker {
+    /**
+     * Массив для хранение заявок.
+     */
+    private final Item[] items = new Item[100];
 
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод реализующий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+
+    }
 }
