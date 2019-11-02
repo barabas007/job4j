8cc49fe2cd55c2e10bde3595ed30c2db40b34e6a
barabas007 <shop.ru.shop>
2019-11-02 13:08:41 +0300

    add Check2

--- chapter_002/src/main/java/ru/job4j/oop/Check.java
+++ chapter_002/src/main/java/ru/job4j/oop/Check.java
@@ -9,28 +9,28 @@ public class Check {
     static boolean check(char[] data) {
 
         for (int i = 0; i < data.length; i++) {
-            while (condition) {
+
                 if (data[i] == '(') {
-                    counter++;
+                    counter ++;
                 } else if (data[i] == ')') {
                     counter--;
-                    if (counter < 0)
-                        return true;
+                    if (counter < 0 )
+                        return false;
                 }
             }
             if (counter == 0) {
-                return false;
+                return true;
 
-            }
+
+            }else return false;
         }
-        return true;
 
-    }
+
 
     public static void main(String[] args) {
         char[] expression = {'(',')'};
-        char[] expressionSecond = {'(', ')', '('};
-        System.out.println(check(expressionSecond));
+        char[] expressionSecond = {'(', ')', '(', '('};
         System.out.println(check(expression));
+        System.out.println(check(expressionSecond));
     }
 }
