2d291342e316bfbbebcdc73472b37735a4c06ae8
barabas007 <shop.ru.shop@mail.ru>
2020-02-14 16:00:01 +0300

    replace test

---
+++ chapter_002/src/test/java/ru/job4/tracker/TrackerTest.java
@@ -0,0 +1,27 @@
+package ru.job4.tracker;
+
+import org.junit.Test;
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.Tracker;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+    @Test
+    public void whenReplace(){
+        Tracker tracker = new Tracker();
+        Item item = new Item("test 2");
+        tracker.replace(id, Item item);
+        Item result = tracker.findById(item.setId();
+    }
+}
